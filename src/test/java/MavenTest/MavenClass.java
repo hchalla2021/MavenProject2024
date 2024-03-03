4c35a437290a40500c1672b532203ed727008986
hchalla2021 <hchalla2020@gmail.com>
2024-03-03 23:10:50 +0530

    Initial commit

---
+++ src/test/java/MavenTest/MavenClass.java
@@ -0,0 +1,10 @@
+package MavenTest;
+
+import org.testng.annotations.Test;
+
+public class MavenClass {
+   @Test
+    public void mavenTest2024() {
+       System.out.println("This is maven class");
+    }
+}

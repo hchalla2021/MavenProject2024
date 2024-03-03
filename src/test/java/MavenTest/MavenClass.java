8406eb17da751536d16a8387cfd2fef0a8b4c995
hchalla2021 <hchalla2020@gmail.com>
2024-03-03 23:44:57 +0530

    New Code Added

--- src/test/java/MavenTest/MavenClass.java
+++ src/test/java/MavenTest/MavenClass.java
@@ -1,10 +1,17 @@
 package MavenTest;
-
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
+import java.time.Duration;
 
 public class MavenClass {
    @Test
     public void mavenTest2024() {
-       System.out.println("This is maven class");
+       System.out.println("This is maven and Selenium class");//
+       WebDriver driver = new ChromeDriver();// chrome browser launch
+       driver.get("https://continentalhospitals.com/");
+       driver.manage().window().maximize();
+       driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(60));
+       driver.close();
     }
 }
